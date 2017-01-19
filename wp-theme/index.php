d7ccf611705c4f87b11386d63f0cb5b32d85d47f
heitorrbarros <heitorrbarros@gmail.com>
2017-01-19 20:42:02 -0300

    Adicionando o cabeçalho e o header

--- wp-theme/index.php
+++ wp-theme/index.php
@@ -114,12 +114,4 @@
     </div>
 
 
-    <!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main·js"></script>
-
-</body>
-
-</html>
+<?php get_footer(); ?>
